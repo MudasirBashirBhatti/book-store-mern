00dee40e1e16d613fcb455ed72d28811a4b4b825
Mudasir <for.all6474@gmail.com>
2024-02-01 01:13:09 +0500

    function to infrom mongoDB disconnected suddenly

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,16 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB);
-    console.log("connected to mongodb");
+    console.log("connected to mongoDB");
   } catch (error) {
     throw error;
   }
 };
+
+// if mongoDB suddenly disconnected it will inform
+mongoose.connection.on("Disconnected", () => {
+  console.log("mongoDB disconnected");
+});
 
 const app = express();
 app.listen(8800, () => {
