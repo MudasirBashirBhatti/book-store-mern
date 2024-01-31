f215c81be3c9d6e21b2269d25ab1f2c4a5402101
Mudasir <for.all6474@gmail.com>
2024-02-01 01:50:55 +0500

    model created for books

---
+++ backend/models/books.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const booksSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Books", booksSchema);
